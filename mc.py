b14a4c64b139822c4149a174223e9e37ee8e9156
sulfurCat12 <mrsimpleanimation@gmail.com>
2025-10-27 19:33:04 +0800

    Updated

--- mc.py
+++ mc.py
@@ -1,16 +1,18 @@
 import json, uuid, os
 
-def manifest():
-  print("MC Pack Template Generator\n")
-  name = input("> Name: ")
-  desc = input("> Description: ")
-  
+name = ''
+desc = ''
+BPorRP = 0
+
+RP_UUID = uuid.uuid4()
+
+def manifest_RP():
   manifest = {
     "format_version": 2,
     "header": {
         "description": desc,
         "name": name,
-        "uuid": str(uuid.uuid4()),
+        "uuid": str(RP_UUID),
         "version": [1, 0, 0],
         "min_engine_version": [ 1, 21, 110 ]
     },
@@ -24,10 +26,81 @@ def manifest():
     ]
   }
 
-  os.mkdir(f'packs/{name}')
-  
-  manifest_json = json.dumps(manifest, indent=2)
-  with open(f'packs/{name}/manifest.json', 'w') as f:
-    f.write(manifest_json)
+  if BPorRP == 1:
+    manifest_json = json.dumps(manifest, indent=2)
+    with open(f'packs/{name}/manifest.json', 'w') as f:
+      f.write(manifest_json)
+  elif BPorRP == 3:
+    os.makedirs(f'packs/{name}/RP', exist_ok=True)
+    manifest_json = json.dumps(manifest, indent=2)
+    with open(f'packs/{name}/RP/manifest.json', 'w') as f:
+      f.write(manifest_json)
 
-manifest()
+def manifest_BP():
+  manifest = {
+    "format_version": 2,
+    "header": {
+        "description": desc,
+        "name": name,
+        "uuid": str(uuid.uuid4()),
+        "version": [1, 0, 0]
+    },
+    "modules": [
+        {
+            "description": desc,
+            "type": "data",
+            "uuid": str(uuid.uuid4()),
+            "version": [1, 0, 0]
+        },
+        {
+            "description": desc,
+            "type": "client_data",
+            "uuid": str(uuid.uuid4()),
+            "version": [1, 0, 0]
+        }
+    ],
+    "dependencies": [
+        {
+            "uuid": str(RP_UUID),
+            "version": [1, 0, 0]
+        }
+    ]
+  }
+
+  if BPorRP == 2:
+    manifest_json = json.dumps(manifest, indent=2)
+    with open(f'packs/{name}/manifest.json', 'w') as f:
+      f.write(manifest_json)
+  elif BPorRP == 3:
+    os.makedirs(f'packs/{name}/BP', exist_ok=True)
+    manifest_json = json.dumps(manifest, indent=2)
+    with open(f'packs/{name}/BP/manifest.json', 'w') as f:
+      f.write(manifest_json)
+
+
+def init():
+  global name, desc, BPorRP
+  print("MC Pack Template Generator\n")
+
+  name = str(input('> Name: '))
+  desc = str(input('> Description: '))
+
+  BPorRP = int(input('> What type of pack do you want to generate? (1/2/3)\n1. Resource Pack\n2. Behaviour Pack\n3. Addon (RP + BP)\n'))
+
+  os.makedirs(f'packs/{name}', exist_ok=True)
+
+  if BPorRP == 1:
+    manifest_RP()
+    print(f'Resource Pack Template successfully generated at packs/{name}')
+  elif BPorRP == 2:
+    manifest_BP()
+    print(f'Behaviour Pack Template successfully generated at packs/{name}')
+  elif BPorRP == 3:
+    manifest_RP()
+    manifest_BP()
+    print(f'Addon Pack Template successfully generated at packs/{name}')
+  else:
+    print('Error; Please enter a valid type')
+
+
+init()
